58c72920b2cac7688595be9e3ea253efeb20a2d3
Nur Annisa Balqis <balqis.nurannisa2004@gmail.com>
2024-10-26 21:46:26 +0700

    Update to add Binding

--- MainActivity.kt
+++ MainActivity.kt
@@ -10,18 +10,19 @@ import com.example.geoboost.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
         binding.enterButton.setOnClickListener {
             val intent = Intent(this, Beranda::class.java)
