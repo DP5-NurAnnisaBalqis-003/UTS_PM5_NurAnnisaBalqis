bbcfae2946e1b310701035b5df944c9023e05e61
Nur Annisa Balqis <balqis.nurannisa2004@gmail.com>
2024-10-26 21:47:45 +0700

    Update to add Binding and mode

--- Beranda.kt
+++ Beranda.kt
@@ -1,26 +1,54 @@
 package com.example.geoboost
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.geoboost.databinding.ActivityBerandaBinding
 
 class Beranda : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBerandaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_beranda)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityBerandaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val enterButton = findViewById<Button>(R.id.enter_button)
 
-        enterButton.setOnClickListener {
+        val sharedPreferences = getSharedPreferences("quiz_prefs", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean("isDarkMode", false)
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkMode) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        )
+
+        binding.themeSwitchButton.text = if (isDarkMode) getString(R.string.light_mode) else getString(R.string.dark_mode)
+
+        binding.themeSwitchButton.setOnClickListener {
+            val isCurrentlyDarkMode = sharedPreferences.getBoolean("isDarkMode", false)
+            if (isCurrentlyDarkMode) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPreferences.edit().putBoolean("isDarkMode", false).apply()
+                binding.themeSwitchButton.text = getString(R.string.dark_mode)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPreferences.edit().putBoolean("isDarkMode", true).apply()
+                binding.themeSwitchButton.text = getString(R.string.light_mode)
+            }
+        }
+
+        binding.buttonPlay.setOnClickListener {
             val intent = Intent(this, Soal::class.java)
             startActivity(intent)
         }
